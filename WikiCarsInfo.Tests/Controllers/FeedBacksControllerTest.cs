ee301aea81ed1746abffaa495502a4754a7c8153
Lokesh <iamlokesh123@gmail.com>
2019-04-06 16:31:01 -0400

    Revert "Revert "added test controllers""
    This reverts commit 192a63936b2010d47b5e776c9ad0e2e3f460394e.

--- WikiCarsInfo.Tests/Controllers/FeedBacksControllerTest.cs
+++ WikiCarsInfo.Tests/Controllers/FeedBacksControllerTest.cs
@@ -12,5 +12,69 @@ namespace WikiCarsInfo.Tests.Controllers
     [TestClass]
     public class FeedBacksControllerTest
     {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Details()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Edit()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
